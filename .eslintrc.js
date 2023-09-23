655b1eaecef9ad37b41f4e2ea0eda9e3499037ca
Nick van Santen <vansantennick@gmail.com>
2023-09-23 22:52:18 +0200

    Configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "prettier",
+    //"plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        indent: ["error", 4],
+        "no-multi-spaces": ["error"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -96,10 +96,10 @@
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
 };
 </script>
 
