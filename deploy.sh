cad3ca02086f16c719b4531b4ea0ebb6ca40e1c9
Nick van Santen <vansantennick@gmail.com>
2023-09-23 22:54:35 +0200

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+GITHUB_REPO="https://github.com/Nickvs99/piano-school"
+BRANCH_NAME="gh-pages"
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# npm run build rebuilds the dist folder everytime and therefore deletes the
+# git repo in the dist folder. Thus the git repo needs to be initialized each time
+git init
+
+git checkout -b $BRANCH_NAME
+
+git add -A
+git commit -m "deploy ${COMMIT_HASH}" 
+
+git remote add origin $GITHUB_REPO
+git push -f origin $BRANCH_NAME
+
+cd ..
