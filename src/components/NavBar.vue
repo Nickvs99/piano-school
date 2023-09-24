316f9be746d8a8b58f99db2f363508dfc48596b0
Nick van Santen <vansantennick@gmail.com>
2023-09-24 17:29:06 +0200

    Updated navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    
+<nav class="navbar">
+    <router-link class="navbar-item" to="/"><HomeIcon class="navbar-svg"/></router-link>
+    <router-link class="navbar-item" to="/about">About</router-link>
+</nav>
+
+</template>
+
+<script>
+
+import HomeIcon from "@/assets/icons/HomeIcon.vue";
+
+export default {
+    name: "NavBar",
+    components: { HomeIcon },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.navbar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    gap: 1rem;
+    padding: 1rem;
+
+    svg {
+        width: 2rem;
+        height: 2rem;
+    }
+}
+
+.navbar-item {
+    display: inline-block;
+}
+
+</style>
