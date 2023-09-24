4f9d8b5015908ef94fbb50b513ec8df358d34637
Nick van Santen <vansantennick@gmail.com>
2023-09-24 16:28:08 +0200

    Refreshes work on github pages for SPA

--- src/router/index.js
+++ src/router/index.js
@@ -23,4 +23,19 @@ const router = createRouter({
     routes,
 });
 
+/**
+ * Since this is a SPA, directly going to a url would not work, when
+ * the site is published on github pages. Instead github pages would throw a 404 error. 
+ * This then displays the 404.html in the public directory. This file redirects to the index
+ * page with a string query. This string query is catched by vue and the user is redirected
+ * accordingly.
+ */
+router.beforeEach(to => {    
+    if (to.query.redirect) {
+        let direct = to.query.redirect.replace(" ", "%20");
+        router.replace(direct);
+        return;
+    }
+});
+
 export default router;
