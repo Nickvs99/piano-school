4940f0decd28a47d11e1d3012d9fdc7fb48c1371
Nick van Santen <vansantennick@gmail.com>
2024-01-03 17:07:53 +0100

    Logic for timer implemented

--- src/views/TimerMenuView.vue
+++ src/views/TimerMenuView.vue
@@ -1,12 +1,30 @@
 <template>
 
-<div>
-    Menu
-</div>
+    <form @submit.prevent="submit">
+        <input v-model="time" type="number">
+        <button>Start</button>  
+    </form>
+
 
 </template>
 
 <script>
+
+export default {
+    data() {
+        return {
+            time: 0,
+        };
+    },
+    methods: {
+        submit() {
+            this.$router.push({
+                name: "timer", 
+                params: { time: this.time }
+            });
+        }
+    }  
+};
 
 </script>
 
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,15 +1,96 @@
 <template>
 
-<div>
-    Timer
+<div id="timer-container" @mousedown="onMouseDown" @mouseup="onMouseUp">
+    <div>
+        {{timeText}}
+    </div>
 </div>
 
 </template>
 
 <script>
+import { sleep } from "@/js/sleep";
 
+export default {
+
+    data() {
+        return {
+            maxTime: 0,
+            timeLeft: 0,
+            start: 0,
+            mousedownTime: 0,
+            holdTimeThreshold: 2500, //ms
+        }; 
+    },
+
+    mounted() {
+        this.maxTime = this.$route.params.time * 1000;
+        this.resetTimer();
+        this.run();
+    },
+
+    computed: {
+        timeText() {
+
+            let time = this.timeLeft / 1000;
+
+            if (time > 10)
+                return time.toFixed(0);
+
+            if(time > 3)
+                return time.toFixed(1);
+
+            if(time <= 0)
+                return 0;
+
+            return time.toFixed(2);
+        }
+    },
+
+    methods: {  
+        async run() {
+            for(;;) {
+                await sleep(0);
+                
+                var delta = Date.now() - this.start; // milliseconds elapsed since start
+
+                this.timeLeft = Math.max(this.maxTime - delta, 0);
+            }
+        },
+
+        resetTimer() {
+            this.start = Date.now();
+            this.timeLeft = this.maxTime;
+        },
+
+        onMouseDown() {
+            this.mouseDownTime = Date.now();            
+        },
+
+        onMouseUp() {
+            let holdTime = Date.now() - this.mouseDownTime;
+
+            if (holdTime > this.holdTimeThreshold) {
+                this.resetTimer();
+                return;
+            }
+
+            if(this.timeLeft <= 0)
+                return;
+
+            this.resetTimer();
+        }
+    }
+
+};
 </script>
 
 <style lang="scss" scoped>
+
+#timer-container {
+    background-color: lightgrey;
+    width: 100vw;
+    height: 100vh;
+}
 
 </style>
